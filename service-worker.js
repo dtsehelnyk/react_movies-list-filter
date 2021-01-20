aca02fb270aaec2db73a5ae6b17bbe419cfc3ce8
Dmytro Tsehelnyk <34zimnii@gmail.com>
2021-01-20 21:25:47 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.4234f62b863ae7f8aa2c014f22378687.js"
 );
 
 self.addEventListener('message', (event) => {
